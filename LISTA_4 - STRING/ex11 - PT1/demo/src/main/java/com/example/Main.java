d2fa4f86ddaeb60e2b52bd93b31d67218351619b
Otávio Augusto Zangelmi Costa <otavioazc@gmail.com>
2025-05-10 13:42:37 -0300

    Update Main.java

--- LISTA_4 - STRING/ex11 - PT1/demo/src/main/java/com/example/Main.java
+++ LISTA_4 - STRING/ex11 - PT1/demo/src/main/java/com/example/Main.java
@@ -1,6 +1,8 @@
 package com.example;
 
 public class Main {
+
+    
     public static void main(String[] args) {
         Documento doc = new Documento("Exemplo", "Este é um exemplo. Exemplo de teste! Sim isso esta cheio de exemplo.");
         System.out.println(doc.contarPalavra("exemplo"));  
